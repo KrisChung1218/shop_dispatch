9b1ea306234b1486824269d34ee2ea657089d789
chsnow <krischung@126.com>
2023-02-27 22:34:51 +0800

    第一次提交

---
+++ src/pages/home/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <view class="content">
+    这里是首页
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const title = ref('Hello')
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+
+</style>
